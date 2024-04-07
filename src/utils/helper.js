c266d151554f11d7755a8d01a0f56816bdcd24eb
Richu Sony <sonyrichu4@gmail.com>
2024-04-07 23:02:58 +0530

    fix: credentials value

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,26 +8,31 @@ export async function loginSignUp(email, fullname, profile_img, setUser) {
     profile_img,
   };
   // await axios.post("/api/auth/login-signup", {});
-  const res = await fetch(`${import.meta.env.VITE_BACKEND_DOMAIN}/api/auth/login-signup`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials:true,
-    body: JSON.stringify(reqData),
-  });
-  console.log("checking login : ",res)
-  if(res.status == 201 || res.status == 200) {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_DOMAIN}/api/auth/login-signup`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials:"include",
+      body: JSON.stringify(reqData),
+    }
+  );
+  console.log("checking login : ", res);
+  if (res.status == 201 || res.status == 200) {
     const resData = await res.json();
-  sessionStorage.setItem("login-user", JSON.stringify(resData));
-  setUser(resData);
+    sessionStorage.setItem("login-user", JSON.stringify(resData));
+    setUser(resData);
   }
   // console.log("response from backend  ", resData);
 }
 
 export async function getUsers() {
   // console.log("reached");
-  const res = await axios.get(`${import.meta.env.VITE_BACKEND_DOMAIN}/api/users/`);
+  const res = await axios.get(
+    `${import.meta.env.VITE_BACKEND_DOMAIN}/api/users/`
+  );
   return res.data;
   // console.log("all users from backend  ", res);
 }
