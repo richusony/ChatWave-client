e14adc6d8fb6d47572e2cb17a17f30b4f0653df1
Richu Sony <sonyrichu4@gmail.com>
2024-04-08 14:55:48 +0530

    Add vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Chats from "./components/Chats.jsx";
 import LoginSignUp from "./components/LoginSignUp.jsx";
-import ChatSec from "./components/ChatSec.jsx";
-import LoggedInUserCnxtProvider from "./context/LoggedInUserCnxtProvider.jsx";
-import { SocketContextProvider } from "./context/SocketContext.jsx";
-import FindUserPage from "./components/FindUserPage.jsx";
 import { MenuContextProvider } from "./context/MenuContext.jsx";
-// import UserContextProvider from "./context/UserContextProvider.jsx";
-// import { ThemeProvdier } from "./context/theme.js";
+import LoggedInUserCnxtProvider from "./context/LoggedInUserCnxtProvider.jsx";
+import "./index.css";
 
 const chatRoute = createBrowserRouter([
   {
     path: "/",
-    element:<LoggedInUserCnxtProvider><LoginSignUp /></LoggedInUserCnxtProvider> ,
+    element: <LoggedInUserCnxtProvider><LoginSignUp /></LoggedInUserCnxtProvider>,
   },
   {
     path: "/chats",
-    element: <MenuContextProvider><LoggedInUserCnxtProvider><Chats /></LoggedInUserCnxtProvider></MenuContextProvider>,
-    // children: [
-    //   {
-    //     path: ":userId", // Use ":userId" for dynamic user IDs
-    //     element: <MenuContextProvider><LoggedInUserCnxtProvider><Chats /></LoggedInUserCnxtProvider></MenuContextProvider>,
-    //   },
-    // ],
+    element: <MenuContextProvider><LoggedInUserCnxtProvider><Chats /></LoggedInUserCnxtProvider></MenuContextProvider>
   },
-  // {
-  //   path: "/mobile/chats/:userId",
-  //   element: <LoggedInUserCnxtProvider><ChatSec /></LoggedInUserCnxtProvider>,
-  // },
   {
     path: "/login",
     element: <LoggedInUserCnxtProvider><LoginSignUp /></LoggedInUserCnxtProvider>,
@@ -44,15 +28,11 @@ const chatRoute = createBrowserRouter([
   {
     path: "*", // Matches any path not explicitly defined above
     element: <Chats />, // Redirect to the root path
-  },
-  // {
-  //   path: "/find",
-  //   element: <LoggedInUserCnxtProvider><FindUserPage /></LoggedInUserCnxtProvider>,
-  // },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-        <RouterProvider router={chatRoute} />
+    <RouterProvider router={chatRoute} />
   </React.StrictMode>
 );
