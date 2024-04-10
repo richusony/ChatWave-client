ffcdaef1d7f97b8b0aa6851eb217be3b565b11fc
Richu Sony <sonyrichu4@gmail.com>
2024-04-10 14:41:31 +0530

    fix: resolve large screen issues

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,7 +5,7 @@ import UsersList from "./UsersList";
 import FindUserPage from "./FindUserPage.jsx"
 import ChatSec from "./ChatSec";
 import InitialPage from "./InitialPage";
-import {SelectedChatProvider, useSelectedChat } from "../context/SelectedChat.jsx";
+import { SelectedChatProvider, useSelectedChat } from "../context/SelectedChat.jsx";
 import { useLoggedInUser } from "../context/LoggedInUserCnxtProvider.jsx";
 import { SocketContextProvider } from "../context/SocketContext.jsx";
 import { useMenuContext } from "../context/MenuContext.jsx";
@@ -15,10 +15,10 @@ import useScreen from "../Hooks/useScreen.js";
 
 
 const Chats = () => {
-  
+
   const { user } = useLoggedInUser();
   const { menuBar } = useMenuContext();
-  const {openWindow, selectedId} = useSelectedChat();
+  const { openWindow, selectedId } = useSelectedChat();
   const screenWidth = useScreen();
 
   // console.log("menu ", menuBar)
@@ -27,17 +27,36 @@ const Chats = () => {
 
   return (
     <AuthContextProvider>
-          <SocketContextProvider>
-            
-              {!user && <Navigate to="/" />}
-              <div className="transition delay-150 ease-linear w-full h-full flex overflow-hidden">
-                {openWindow && <FindUserPage />}
-                {menuBar ? <MenuBar /> : screenWidth < 768  && !selectedId && <UsersList /> }
-                <div className={`${selectedId && screenWidth < 768 ? "" :"hidden"} md:block w-full md:w-2/3 h-full bg-[#E8E8F9]`}>
-                  {selectedId ?<ChatSec />: <InitialPage />}
+      <SocketContextProvider>
+
+        {!user && <Navigate to="/" />}
+        <div className="transition delay-150 ease-linear w-full h-full flex overflow-hidden">
+          {openWindow && <FindUserPage />}
+
+          {screenWidth < 768 ? (<>
+            {
+              menuBar ?
+                <MenuBar /> :
+                selectedId == null ?
+                  <UsersList /> :
+                  <div className={`w-full md:w-2/3 h-full bg-[#E8E8F9]`}>
+                    <ChatSec />
+                  </div>
+            }
+          </>
+          )
+            :
+            (
+              <>{menuBar ? <MenuBar /> : <UsersList />}
+                <div className={`hidden md:block w-full md:w-2/3 h-full bg-[#E8E8F9]`}>
+                  {selectedId ? <ChatSec /> : <InitialPage />}
                 </div>
-              </div>
-          </SocketContextProvider>
+              </>)
+          }
+
+
+        </div>
+      </SocketContextProvider>
     </AuthContextProvider>
   );
 };
